8a2df85b66d8ea0c5219f964691db0d1f05f0996
Christie Wilson <bobcatfish@gmail.com>
2019-11-08 13:47:54 -0500

    Use templating to generate the page
    So I have something to unit test XD

---
+++ cat/cat.go
@@ -0,0 +1,36 @@
+package cat
+
+// Cat holds information about a cat, such as my adorable little monster Yoshimi!
+type Cat struct {
+	// Name is the name of this cat.
+	Name string
+
+	// Mood is the main mood this cat displays.
+	Mood string
+
+	// Image is the path to an image on disk that represents this cat.
+	Image string
+
+	// Age is the number of cat years old this cat is.
+	Age int
+
+	// Pronoun is the personal pronoun to use for this cat.
+	Pronoun string
+}
+
+// AgeInCatYears will look at how old c is in human years and convert that to the
+// cat years equivalent.
+func AgeInCatYears(hoomanYears int) int {
+	return hoomanYears
+}
+
+// Yoshimi is my ancient cat.
+func Yoshimi() *Cat {
+	return &Cat{
+		Name:    "Yoshimi",
+		Mood:    "grouchy",
+		Image:   "yoshimi.jpg",
+		Age:     AgeInCatYears(17),
+		Pronoun: "She",
+	}
+}
--- main.go
+++ main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/bobcatfish/catservice/cat"
 )
+
+func sweetCatBlog(w http.ResponseWriter, r *http.Request) {
+	y := cat.Yoshimi()
+
+	tmpl := template.Must(template.ParseFiles("static/index.html"))
+	tmpl.Execute(w, y)
+}
 
 func main() {
 	port := flag.Int("port", 80, "The port to listen on")
 	flag.Parse()
 
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/", sweetCatBlog)
 	log.Printf("Starting on port %d", *port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
