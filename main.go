bed5a5c10526eab19ab30003dc430b52e4cd2e39
Christie Wilson <bobcatfish@gmail.com>
2019-11-08 13:26:02 -0500

    Make port configurable
    Was for integration tests but ive given up on those for now.

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-    http.Handle("/", http.FileServer(http.Dir("./static")))
-    log.Fatal(http.ListenAndServe(":80", nil))
+	port := flag.Int("port", 80, "The port to listen on")
+	flag.Parse()
 
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	log.Printf("Starting on port %d", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
