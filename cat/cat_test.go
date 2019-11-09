ce8208da686ff85d4e2aa4aa61b85d4175df0b49
Christie Wilson <bobcatfish@gmail.com>
2019-11-09 18:28:44 -0500

    Introduce failure on purpose

--- cat/cat_test.go
+++ cat/cat_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestAgeInCatYears(t *testing.T) {
 	age := AgeInCatYears(20)
-	expectedAge := 140
+	expectedAge := 141
 	if age != expectedAge {
 		t.Errorf("Expected age %d but got %d", age, expectedAge)
 	}
