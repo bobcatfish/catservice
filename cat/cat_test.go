e58e912c9abd3153927ec99fc67cf7bd3ae96c4f
Christie Wilson <bobcatfish@gmail.com>
2019-11-09 19:33:11 -0500

    Fix the test
    I'd just broken it to make the check fail :D

--- cat/cat_test.go
+++ cat/cat_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestAgeInCatYears(t *testing.T) {
 	age := AgeInCatYears(20)
-	expectedAge := 141
+	expectedAge := 140
 	if age != expectedAge {
 		t.Errorf("Expected age %d but got %d", age, expectedAge)
 	}
