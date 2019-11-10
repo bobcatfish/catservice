cf26f836e20bbb7ea296866d62f30b95158b30c6
Christie Wilson <bobcatfish@gmail.com>
2019-11-10 11:59:27 -0500

    Fix the image and add more tests
    More tests that I'm gonna break when I make the logic more sophisticated
    :D

--- cat/cat_test.go
+++ cat/cat_test.go
@@ -1,11 +1,35 @@
 package cat
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAgeInCatYears(t *testing.T) {
 	age := AgeInCatYears(20)
 	expectedAge := 140
 	if age != expectedAge {
 		t.Errorf("Expected age %d but got %d", age, expectedAge)
+	}
+	tests := []struct {
+		humanYears int
+		catYears   int
+	}{{
+		humanYears: 20,
+		catYears:   140,
+	}, {
+		humanYears: 1,
+		catYears:   7,
+	}, {
+		humanYears: 4,
+		catYears:   28,
+	}}
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%d", tc.humanYears), func(t *testing.T) {
+			actual := AgeInCatYears(tc.humanYears)
+			if actual != tc.catYears {
+				t.Errorf("Expected %d in cat years when %d in human years but got %d", tc.catYears, tc.humanYears, actual)
+			}
+		})
 	}
 }
